6c9ebea5df1a9afafa031e5bcbab8d2ce5f5217d
kirsenkodmitry <kirsenkodmitry@gmail.com>
2025-09-29 15:33:14 +0300

    init commit

---
+++ src/main/java/ru/vlsu/groupPurchasing/GroupPurchasingApplication.java
@@ -0,0 +1,13 @@
+package ru.vlsu.groupPurchasing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupPurchasingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupPurchasingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vlsu/groupPurchasing/GroupPurchasingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlsu.groupPurchasing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupPurchasingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
